fe90d59281c139011316df4a54e1a5261285ff1c
robin <robin@robins-MacBook-Air.local>
2025-09-21 16:31:38 +0200

    10-spring-intro done

--- lab/10-spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
+++ lab/10-spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
@@ -3,9 +3,13 @@ package rewards.internal;
 import rewards.Dining;
 import rewards.RewardConfirmation;
 import rewards.RewardNetwork;
+import rewards.AccountContribution;
+import rewards.internal.account.Account;
 import rewards.internal.account.AccountRepository;
+import rewards.internal.restaurant.Restaurant;
 import rewards.internal.restaurant.RestaurantRepository;
 import rewards.internal.reward.RewardRepository;
+import common.money.MonetaryAmount;
 
 /**
  * Rewards an Account for Dining at a Restaurant.
@@ -15,18 +19,6 @@ import rewards.internal.reward.RewardRepository;
  * 
  * Said in other words, this class implements the "reward account for dining" use case.
  *
- * TODO-00: In this lab, you are going to exercise the following:
- * - Understanding internal operations that need to be performed to implement
- *   "rewardAccountFor" method of the "RewardNetworkImpl" class
- * - Writing test code using stub implementations of dependencies
- * - Writing both target code and test code without using Spring framework
- *
- * TODO-01: Review the Rewards Application document (Refer to the lab document)
- * TODO-02: Review project dependencies (Refer to the lab document)
- * TODO-03: Review Rewards Commons project (Refer to the lab document)
- * TODO-04: Review RewardNetwork interface and RewardNetworkImpl class below
- * TODO-05: Review the RewardNetworkImpl configuration logic (Refer to the lab document)
- * TODO-06: Review sequence diagram (Refer to the lab document)
  */
 public class RewardNetworkImpl implements RewardNetwork {
 
@@ -50,9 +42,12 @@ public class RewardNetworkImpl implements RewardNetwork {
 	}
 
 	public RewardConfirmation rewardAccountFor(Dining dining) {
-		// TODO-07: Write code here for rewarding an account according to
-		//          the sequence diagram in the lab document
-		// TODO-08: Return the corresponding reward confirmation
-		return null;
+		Account account = accountRepository.findByCreditCard(dining.getCreditCardNumber());
+		Restaurant restaurant = restaurantRepository.findByMerchantNumber(dining.getMerchantNumber());
+		MonetaryAmount amount = restaurant.calculateBenefitFor(account, dining);
+		AccountContribution contribution = account.makeContribution(amount);
+		accountRepository.updateBeneficiaries(account);
+
+		return rewardRepository.confirmReward(contribution, dining);
 	}
 }
